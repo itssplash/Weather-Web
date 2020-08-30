ecdb34eb36ad5a90dd70c9f08d593da69e034fc8
unknown <abhinaavv007@gmail.com>
2020-08-30 20:28:26 +0530

    Updates

---
+++ precache-manifest.5a14c5c394b725925ebbef374434019b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be6bfb74794127a25469dd976766619d",
+    "url": "/Weather-Web/index.html"
+  },
+  {
+    "revision": "21a6ace9b66dae88b2c3",
+    "url": "/Weather-Web/static/css/main.5cae10e4.chunk.css"
+  },
+  {
+    "revision": "4a79708fe2bb8d98f2f2",
+    "url": "/Weather-Web/static/js/2.07144284.chunk.js"
+  },
+  {
+    "revision": "85b12af60ddf57ea6860c3b16407fd52",
+    "url": "/Weather-Web/static/js/2.07144284.chunk.js.LICENSE"
+  },
+  {
+    "revision": "21a6ace9b66dae88b2c3",
+    "url": "/Weather-Web/static/js/main.c85b6d79.chunk.js"
+  },
+  {
+    "revision": "a0165ada7cfcc0d04102",
+    "url": "/Weather-Web/static/js/runtime-main.b71b9a1e.js"
+  },
+  {
+    "revision": "c97cb84b74a52edcb496295687e87822",
+    "url": "/Weather-Web/static/media/cold-bg.c97cb84b.jpg"
+  },
+  {
+    "revision": "635ed87d2cc826fb213057ac40dfaff1",
+    "url": "/Weather-Web/static/media/warm-bg.635ed87d.jpg"
+  }
+]);
